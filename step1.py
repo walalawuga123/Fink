6b247d484a6c22cbc429b9f7c9ee7a26fda356cc
walalawuga123 <issacwei333@gmail.com>
2025-03-26 11:57:28 -0500

    Update step1.py

--- step1.py
+++ step1.py
@@ -11,11 +11,15 @@ input_DVoffset = widgets.Text(description="DV offset: ")
 display(VBox([input_box1, input_box2]))
 
 # Function to capture and print the inputs when the button is clicked
-def get_inputs(b):
+def AngleCorrection(b):
   RCSL = input_RCSL.value
   DVoffset = input_DVoffset.value
+  PitchCorrection = round(math.atan(-DVoffset/RCSL)*180/3.14159, 2)
+  print(f'Advise angle correction: {PitchCorrection}')
   
 # Create and display a submit button
 button = widgets.Button(description="Submit")
-button.on_click(get_inputs)
+button.on_click(AngleCorrection)
 display(button)
+
+
