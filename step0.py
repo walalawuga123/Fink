b090eb6d74b80a15b7facb198a48b748c7e08673
walalawuga123 <issacwei333@gmail.com>
2025-03-26 12:47:24 -0500

    Update step0.py

--- step0.py
+++ step0.py
@@ -9,7 +9,7 @@ drive.mount('/content/drive')
 auth.authenticate_user() # Authenticate manually to avoid errors
 
 # Load and modify the Excel file
-https://docs.google.com/spreadsheets/d/1e7ZqoPTFdUS_E163w0M41izWisuC9tJd/edit?usp=sharing&ouid=114104750852824675713&rtpof=true&sd=true
+
 file_id = '1e7ZqoPTFdUS_E163w0M41izWisuC9tJd' # need to be change after switch files
 filename = gdown.download(f"https://drive.google.com/uc?id={file_id}", "downloaded_file.xlsx", quiet=False)
 head_parameter = pd.read_excel(filename, sheet_name='Sheet1')
