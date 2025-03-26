d42ffd3a83e038ded1ac7b01520415d5186c1a09
walalawuga123 <issacwei333@gmail.com>
2025-03-26 13:00:20 -0500

    Update step0.py

--- step0.py
+++ step0.py
@@ -5,8 +5,13 @@ from google.colab import drive
 from gspread_dataframe import set_with_dataframe
 from google.auth import default
 from google.colab import auth
+
 drive.mount('/content/drive')
 auth.authenticate_user() # Authenticate manually to avoid errors
+
+# Get authenticated credentials
+creds, _ = default()
+gc = gspread.authorize(creds)
 
 # Load and modify the Excel file
 
@@ -15,7 +20,6 @@ filename = gdown.download(f"https://drive.google.com/uc?id={file_id}", "download
 head_parameter = pd.read_excel(filename, sheet_name='Sheet1')
 new_mouse_id = str(input("Mouse ID: "))
 head_parameter[new_mouse_id] = ""
-print(head_parameter[new_mouse_id])
 
 # Open the existing Google Sheet using its file ID
 sh = gc.open_by_key(file_id)  # Open the Google Sheet with the same file_id
