d36901b29c1cd6d2c8c136b64ef5338f83b4ffff
walalawuga123 <issacwei333@gmail.com>
2025-03-26 13:36:02 -0500

    Update step0.py

--- step0.py
+++ step0.py
@@ -1,26 +1,24 @@
 import gspread
 import pandas as pd
-import gdown
 from google.colab import drive
 from gspread_dataframe import set_with_dataframe
 from google.auth import default
 from google.colab import auth
 
+# Mount Google Drive and authenticate
 drive.mount('/content/drive')
-auth.authenticate_user() # Authenticate manually to avoid errors
-
+auth.authenticate_user()  # Authenticate manually to avoid errors
 # Get authenticated credentials
 creds, _ = default()
 gc = gspread.authorize(creds)
 
-file_id = '1e7ZqoPTFdUS_E163w0M41izWisuC9tJd' # need to be change after switch files
-filename = gdown.download(f"https://drive.google.com/uc?id={file_id}", "downloaded_file.xlsx", quiet=False)
-head_parameter = pd.read_excel(filename, sheet_name='Sheet1')
+file_id = '17t6CB6Nze274z1od3cmfdKnHZ2OMLdFFay7yMQ_Ofi0'  # Use the correct Google Sheet ID here
+sh = gc.open_by_key(file_id)  # Open the Google Sheet with the file_id
+worksheet = sh.get_worksheet(0)  # Select the first sheet
+
+head_parameter = pd.DataFrame(worksheet.get_all_records())  # Fetch all records from the sheet
 new_mouse_id = str(input("Mouse ID: "))
 head_parameter[new_mouse_id] = ""
 
-# Try opening the Google Sheet
-sh = gc.open_by_key(file_id)
-worksheet = sh.get_worksheet(0)
 worksheet.clear()
-set_with_dataframe(worksheet, df)
+set_with_dataframe(worksheet, head_parameter)
