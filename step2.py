3319450c3e61dc1d6101330c02fd530165f32a7f
walalawuga123 <issacwei333@gmail.com>
2025-03-27 11:23:27 -0500

    Update step2.py

--- step2.py
+++ step2.py
@@ -125,6 +125,14 @@ def create_input_boxes():
     }
 
     finishButton.onclick = function() {
+        var val1 = document.getElementById("xR1000").value;
+        var val2 = document.getElementById("xR3000").value;
+        var val3 = document.getElementById("zR1000").value;
+        var val4 = document.getElementById("zR3000").value;
+        var val5 = document.getElementById("xL1000").value;
+        var val6 = document.getElementById("xL3000").value;
+        var val7 = document.getElementById("zL1000").value;
+        var val8 = document.getElementById("zL3000").value;
         google.colab.kernel.invokeFunction("notebook.finish_correction", [val1, val2, val3, val4, val5, val6, val7, val8], {});
     }
     '''))
